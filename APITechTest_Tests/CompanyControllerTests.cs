358a0c3736c29f1acc32ce635363676545f76e2d
Ghanshyam Parekh <ghanshyam.parekh@acfconsultants.com>
2023-03-09 12:15:20 +0000

    - Unit test (xUnit) project added - one basic test created

---
+++ APITechTest_Tests/CompanyControllerTests.cs
@@ -0,0 +1,52 @@
+using APITechTest.Controllers;
+using APITechTest.DataModel;
+using APITechTest.Repositories;
+using Castle.Core.Resource;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata;
+
+namespace APITechTest_Tests
+{
+    public class CompanyControllerTests
+    {     
+        public CompanyControllerTests()
+        {   
+        }
+
+        [Fact]
+        public void GetClaimsByCompanyId_WhenCalled_ReturnsOkResult()
+        {
+            // Arrange
+            int comapnyid = 1;
+            var claims = new List<ClaimDataModel>
+                {
+                    new ClaimDataModel
+                    {
+                        UCR = "UCR 1",
+                        CompanyId =1,
+                        ClaimDate = DateTime.Now.AddDays(-30),
+                        LossDate = DateTime.Now,
+                        AssuredName ="Assured Name 1",
+                        IncurredLoss =Convert.ToDecimal("100.89"),
+                        Closed = true,
+                    },
+                };
+
+            var repositoryMock = new Mock<ICompanyRepository>();
+            repositoryMock
+                .Setup(r => r.GetClaimsByCompanyId(comapnyid))
+                .Returns(claims);
+
+            var controller = new CompanyController(repositoryMock.Object);
+
+            // Act
+            var okResult = controller.GetClaimsByCompanyId(comapnyid);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult as OkObjectResult);
+        }
+    }
+}
